229a02d0f33168cbf5f0e590f30d4719c993be03
belkarx <84473367+belkarx@users.noreply.github.com>
2022-07-16 17:31:21 -0700

    before stripping

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-#!/usr/bin/env rust-script
-
-mod client;
 use std::thread;
 use clap::Parser;
 use std::net::{IpAddr, TcpStream, TcpListener};
 use std::fs;
 
-use std::io::Write;
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+
+use std::io::Read;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
@@ -18,11 +21,43 @@ struct Cli {
 }
 
 fn main() {
+    //crossterm::terminal::enable_raw_mode().unwrap();
     //let port = client::notmain();
-    let addr = fs::read_to_string("test.txt").expect("Error in reading the file");
-    println!("{}", addr);
-    let mut stream = TcpStream::connect(addr).unwrap();
-    std::io::copy(&mut std::io::stdin(), &mut stream).unwrap();
+    //let addr = fs::read_to_string("/tmp/talk.txt").expect("Error in reading the file");
+    //println!("{}", addr);
+    let mut stream = TcpStream::connect("127.0.0.1:1337").unwrap();
+    
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    termion::cursor::Goto(1, 1);
+    write!(stdout, "{}", termion::clear::All).unwrap();
+    stdout.flush().unwrap();
+    let mut i = 0;
+
+    //detecting keydown events
+    let mut k: char;
+    for c in stdin.keys() {
+        k = match c {
+            Ok(Key::Char(e)) => e,
+            _ => '\n'
+        };
+        println!("{}",k);
+        stream.write(&mut [k as u8]).unwrap();
+        
+        i += 1;
+        if i == 20 {
+            break;
+        }
+    }
+    stdout.flush().unwrap();
+
+
+    //let mut stream = std::io::BufWriter::with_capacity(1, stream);
+    //let buf:u8 = 0;
+    //std::io::stdin().read_exact(&mut [buf]).unwrap();
+    //stream.write(&mut [buf]).unwrap();
+//    std::io::copy(&mut std::io::stdin(), &mut stream).unwrap();
+    //crossterm::terminal::disable_raw_mode().unwrap();
 /*
     let cli = Cli::parse();
 
@@ -46,3 +81,4 @@ fn main() {
     }*/
     Ok(())*/
 }
+
