6fee12c0895adb8dc1406ee6abd07c91fbfb3201
belkarx <84473367+belkarx@users.noreply.github.com>
2022-07-16 17:56:14 -0700

    it works

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-use std::thread;
 use clap::Parser;
-use std::net::{IpAddr, TcpStream, TcpListener};
-use std::fs;
+use std::net::{IpAddr, TcpStream};
 
-use std::io::{stdin, stdout, Write};
+use std::io::{self, stdin, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-
-use std::io::Read;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
@@ -20,65 +16,28 @@ struct Cli {
     port: u16,
 }
 
-fn main() {
-    //crossterm::terminal::enable_raw_mode().unwrap();
-    //let port = client::notmain();
-    //let addr = fs::read_to_string("/tmp/talk.txt").expect("Error in reading the file");
-    //println!("{}", addr);
-    let mut stream = TcpStream::connect("127.0.0.1:1337").unwrap();
-    
+fn main() -> io::Result<()> {
+    let cli = Cli::parse();
+    println!("// SENDING CLIENT //");
+    println!("<Esc> to quit");
+    let mut stream = TcpStream::connect((cli.ip.unwrap(), cli.port)).expect("Run the server/listening client first!");
     let stdin = stdin();
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    termion::cursor::Goto(1, 1);
-    write!(stdout, "{}", termion::clear::All).unwrap();
-    stdout.flush().unwrap();
-    let mut i = 0;
+    let mut stdout = stdout().into_raw_mode()?;
+    //termion::cursor::Goto(1, 1);
+    //write!(stdout, "{}", termion::clear::All).unwrap();
+    stdout.flush()?;
 
     //detecting keydown events
     let mut k: char;
     for c in stdin.keys() {
         k = match c {
             Ok(Key::Char(e)) => e,
-            _ => '\n'
+            Ok(Key::Esc) => break,
+            _ => '\n',
         };
-        println!("{}",k);
-        stream.write(&mut [k as u8]).unwrap();
-        
-        i += 1;
-        if i == 20 {
-            break;
-        }
+        println!("{}", k);
+        stream.write(&mut [k as u8])?;
     }
-    stdout.flush().unwrap();
-
-
-    //let mut stream = std::io::BufWriter::with_capacity(1, stream);
-    //let buf:u8 = 0;
-    //std::io::stdin().read_exact(&mut [buf]).unwrap();
-    //stream.write(&mut [buf]).unwrap();
-//    std::io::copy(&mut std::io::stdin(), &mut stream).unwrap();
-    //crossterm::terminal::disable_raw_mode().unwrap();
-/*
-    let cli = Cli::parse();
-
-    println!("port: {}", cli.port);
-    println!("Ip: {:?}", cli.ip.unwrap()); //unwrap will not fail because clap validates the IP
-    
-
-    //stream.set_nonblocking(true)?;
-    let mut stream = TcpStream::connect((cli.ip.unwrap(), cli.port))?;
-    loop {
-        //io::copy(&mut io::stdin().lock(), &mut stream)?;
-        io::copy(&mut stream, &mut io::stdout())?;
-        if 1==2 {
-            break;
-        }
-    }
-
-/*
-    if let Ok(stream2) = TcpStream::connect((cli.ip.unwrap(), cli.port)) {
-        println!("Connected to 1");
-    }*/
-    Ok(())*/
+    stdout.flush()?;
+    Ok(())
 }
-
