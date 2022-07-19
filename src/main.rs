e5c0228800e914ce8867e3d6783e1aa09c454bdd
belkarx <84473367+belkarx@users.noreply.github.com>
2022-07-19 11:03:04 -0700

    windows

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::net::{IpAddr, TcpStream, TcpListener};
 use std::io::*;
 use ncurses::*;
 use std::thread;
-use std::sync::mpsc::channel;
+use std::sync::{Arc, mpsc::channel};
 use std::net;
 
 #[derive(Parser, Debug)]
@@ -21,75 +21,82 @@ struct Cli {
 
 }
 
-fn set_up_tui() -> WINDOW {
+fn set_up_tui(lines: i32, cols: i32) {
     initscr();
-    let lines = LINES();
-    let cols = COLS();
-    let w = newwin(lines, cols, 0, 0);
     mvvline(0, cols/2, '+' as u32, lines);
-    keypad(w, true);
-    mvaddstr(lines-1, (cols/2)+3, "them> ");
-    mvaddstr(lines-1, 1, "me (q to quit)> ");
+    mvaddstr(1, 1, "me (q to quit)> ");
+    mvaddstr(1, (cols/2)+3, "them> ");
     refresh();
-    w
 }
 
 
 fn main() /*-> io::Result<()>*/ {
     let cli = Cli::parse();
-    let (mut stream, addr) = get_stream(cli);
+    let ip = cli.ip.unwrap(); 
+    let lines = LINES();
+    let cols = COLS();
 
-    let mut addr = net::SocketAddr::new(cli.ip.unwrap(), 1337);
-    let (stream, addr) = match TcpStream::connect(addr) {
-        Ok(stream) => (stream, addr),
-        Err(e) => {
-            println!("LISTENING - {:?}", e);
-            TcpListener::bind((cli.ip.unwrap(), 1331))
-            .unwrap()
-            .accept()
-            .unwrap()
-        }
-    };
+    let (mut stream, addr) = get_stream(ip);
+    //let stream = Arc::new(stream);
 
     println!("{:?}, {:?}", stream, addr);
 
-   /* 
-    let w = set_up_tui();
-    //std::thread::sleep(std::time::Duration::from_secs(5));
+    set_up_tui(lines, cols);
+    
+    let mut stream2 = stream.try_clone().unwrap();
+    
+    //recv
+    thread::spawn(move|| {
+        let w = newwin(lines, cols, 0, 0);
+        //let mut i = 0;
+        //let mut k:u8 = 0;
+        let mut buf = [0; 1];
+        loop {
+            stream2.read_exact(&mut buf).unwrap();
+            addch(w, buf[0] as u32);
+            refresh();
+        }
+    });
+
     //send
     thread::spawn(move || {  
         let mut k: i32 = 0;   
         while k != 113 {
             k = getch();
+            /*if k == KEY_BACKSPACE {
+                k = 10;
+            }*/
             stream.write(&mut [k as u8]).unwrap();
             refresh();
         }
     });
     
-    //recv
-    thread::spawn(move|| {
-       std::io::copy(&mut stream, &mut std::io::stdout());
-    });
 
-
+    std::thread::sleep(std::time::Duration::from_secs(5));
 
     //clean up TUI
-    delwin(w);
     endwin();
-    refresh();*/
+    refresh();
 }
 
-
-fn get_stream(cli: Cli) -> (TcpStream, net::SocketAddr) {
-    let addr = net::SocketAddr::new(cli.ip.unwrap(), 1337);
+fn get_stream(ip: IpAddr) -> (TcpStream, net::SocketAddr) {
+    let addr = net::SocketAddr::new(ip, 1337);
     match TcpStream::connect(addr) {
-        Ok(stream) => (stream, ()),
+        Ok(stream) => (stream, addr),
         Err(e) => {
             println!("LISTENING - {:?}", e);
-            TcpListener::bind((cli.ip.unwrap(), 1331))
+            TcpListener::bind(addr)
             .unwrap()
             .accept()
             .unwrap()
         }
     }
 }
+
+/*
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn 
+}*/
