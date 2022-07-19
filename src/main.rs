09e0d387e9d9a3fc474e3fee31639a49c6654623
belkarx <84473367+belkarx@users.noreply.github.com>
2022-07-19 14:03:51 -0700

    technically should work now

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,38 @@
 use clap::Parser;
-use std::net::{IpAddr, TcpStream, TcpListener};
+use std::net::{IpAddr, TcpListener, TcpStream};
 
-use std::io::*;
 use ncurses::*;
-use std::thread;
-use std::sync::{Arc, mpsc::channel};
+use std::io::*;
 use std::net;
+use std::thread;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
     #[clap(short, long, value_parser, required = true, value_hint = clap::ValueHint::Hostname)]
     ip: Option<IpAddr>,
-
     /*#[clap(short, long, value_parser = clap::value_parser!(u16).range(1..), default_value_t = 1331)]
     lport: u16,
 
     #[clap(short, long, value_parser = clap::value_parser!(u16).range(1..), default_value_t = 1337)]
     rport: u16,*/
-
 }
 
-fn set_up_tui(lines: i32, cols: i32) {
+fn set_up_tui(lines: i32, cols: i32) -> WINDOW {
     initscr();
-    mvvline(0, cols/2, '+' as u32, lines);
+    let w = newwin(lines, cols, 0, 0);
+    keypad(w, true);
+    start_color();
+    use_default_colors();
     mvaddstr(1, 1, "me (q to quit)> ");
-    mvaddstr(1, (cols/2)+3, "them> ");
     refresh();
+    w
 }
 
-
-fn main() /*-> io::Result<()>*/ {
+fn main() /*-> io::Result<()>*/
+{
     let cli = Cli::parse();
-    let ip = cli.ip.unwrap(); 
+    let ip = cli.ip.unwrap();
     let lines = LINES();
     let cols = COLS();
 
@@ -41,40 +41,47 @@ fn main() /*-> io::Result<()>*/ {
 
     println!("{:?}, {:?}", stream, addr);
 
-    set_up_tui(lines, cols);
-    
+    let w = set_up_tui(lines, cols);
+
     let mut stream2 = stream.try_clone().unwrap();
-    
-    //recv
-    thread::spawn(move|| {
-        let w = newwin(lines, cols, 0, 0);
-        //let mut i = 0;
-        //let mut k:u8 = 0;
-        let mut buf = [0; 1];
-        loop {
-            stream2.read_exact(&mut buf).unwrap();
-            addch(w, buf[0] as u32);
-            refresh();
-        }
-    });
+    mv(1, 18);
 
     //send
-    thread::spawn(move || {  
-        let mut k: i32 = 0;   
-        while k != 113 {
+    let res = thread::spawn(move || {
+        let mut k;
+        loop {
             k = getch();
-            /*if k == KEY_BACKSPACE {
-                k = 10;
-            }*/
+            if k == 10 {
+                insertln();
+            }
             stream.write(&mut [k as u8]).unwrap();
             refresh();
         }
     });
-    
 
-    std::thread::sleep(std::time::Duration::from_secs(5));
+    //recv
+    thread::spawn(move || {
+        init_pair(1, COLOR_WHITE, COLOR_BLUE);
+        let mut buf = [0; 1];
+        loop {
+            stream2.read_exact(&mut buf).unwrap();
+            //println!("{:?}", buf);
+            if buf[0] == 10 {
+                //newline
+                insertln();
+            } else {
+                attron(COLOR_PAIR(1));
+                addch(buf[0] as u32);
+                attroff(COLOR_PAIR(1));
+            }
+            refresh();
+        }
+    });
+
+    let _ = res.join();
 
     //clean up TUI
+    delwin(w);
     endwin();
     refresh();
 }
@@ -85,10 +92,7 @@ fn get_stream(ip: IpAddr) -> (TcpStream, net::SocketAddr) {
         Ok(stream) => (stream, addr),
         Err(e) => {
             println!("LISTENING - {:?}", e);
-            TcpListener::bind(addr)
-            .unwrap()
-            .accept()
-            .unwrap()
+            TcpListener::bind(addr).unwrap().accept().unwrap()
         }
     }
 }
@@ -98,5 +102,5 @@ fn get_stream(ip: IpAddr) -> (TcpStream, net::SocketAddr) {
 mod tests {
     use super::*;
     #[test]
-    fn 
+    fn
 }*/
